1e52af2737320011312f0ed63cc614e019d8a1d0
thanks299 <thanksagbeble@gmail.com>
2024-08-05 11:18:48 -0700

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Task 0 module"""
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
